af977c3c85562251b6c55b0053ceb876cfba36e8
makspanshin <makspanshin97@gmail.com>
2023-05-02 19:24:54 +0500

    Update Program.cs

--- DebugProject/Program.cs
+++ DebugProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ToDoListBot.DAL;
 using ToDoListBot.Model;
+using ToDoListManagement;
 using ToDoListManagement.Storage;
 
 
@@ -9,63 +10,33 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //var doItem1 = new ToDoItem("Сделать", "To do porgramm");
-        //var doItem2 = new ToDoItem("Сделать1", "To do porgramm1");
-        //var doItem3 = new ToDoItem("Сделать2", "To do porgramm2");
 
-        //var manager = new ToDoListManager(new StorageTasks());
-
-        //manager.Add(doItem1);
-        //manager.Add(doItem2);
-        //manager.Add(doItem3);
-
-        //var list = manager.GetAllTasks();
-        //var index = 0;
-        //foreach (var item in list)
+        //using (ApplicationContext db = new ApplicationContext())
         //{
-        //    Console.WriteLine(index + ":" + item.ShortName + ":" + item.Description + "-" + item.IsDone);
-        //    index++;
+        //    // создаем два объекта User
+        //    var user1 = new User { NickName = "Tom",UserId = 1};
+        //    var user2 = new User { NickName = "Alice", UserId = 2};
+
+        //    // добавляем их в бд
+        //    //db.Users.AddRange(user1, user2);
+        //    //db.SaveChanges();
+
+        //    db.ToDoItem.Add(new ToDoItem() {DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd",UserId = 1});
+        //    db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
+        //    db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
+        //    db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
+        //    db.SaveChanges();
+
+        //    var ttt = db.Users.Where(user => user.NickName == "Tom").ToList().First().Tasks;
         //}
 
-        //Console.WriteLine("------------------------------------------------------------------");
-        //manager.СompleteTask(2);
-        //index = 0;
-        //foreach (var item in list)
-        //{
-        //    Console.WriteLine(index + ":" + item.ShortName + ":" + item.Description + "-" + item.IsDone);
-        //    index++;
-        //}
+        //StorageTasks maStorageTasks = new StorageTasks(new ApplicationContext());
 
-        //manager.Delete(2);
-        //Console.WriteLine("------------------------------------------------------------------");
-        //index = 0;
-        //foreach (var item in list)
-        //{
-        //    Console.WriteLine(index + ":" + item.ShortName + ":" + item.Description + "-" + item.IsDone);
-        //    index++;
-        //}
+        //var trstList = maStorageTasks.GetTasks("Tom");
 
-        using (ApplicationContext db = new ApplicationContext())
-        {
-            // создаем два объекта User
-            var user1 = new User { NickName = "Tom",UserId = 1};
-            var user2 = new User { NickName = "Alice", UserId = 2};
+        ToDoListManager toDoListManager = new ToDoListManager(new StorageTasks(new ApplicationContext()));
 
-            // добавляем их в бд
-            //db.Users.AddRange(user1, user2);
-            //db.SaveChanges();
+        toDoListManager.СompleteTask("pinguin007",0);
 
-            db.ToDoItem.Add(new ToDoItem() {DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd",UserId = 1});
-            db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
-            db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
-            db.ToDoItem.Add(new ToDoItem() { DateCreate = DateTime.Now.ToUniversalTime(), Description = "Lfdflq dasdasd", UserId = 1 });
-            db.SaveChanges();
-
-            var ttt = db.Users.Where(user => user.NickName == "Tom").ToList().First().Tasks;
-        }
-
-        StorageTasks maStorageTasks = new StorageTasks(new ApplicationContext());
-
-        var trstList = maStorageTasks.GetTasks("Tom");
     }
 }
